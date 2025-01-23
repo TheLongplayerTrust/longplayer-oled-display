5e3a8481771103f6f03c1248df4432ea619f853c
Daniel Jones <daniel@jones.org.uk>
2025-01-23 01:09:48 +0000

    Add progressive display for ntp/satellite sync; add ticker

--- display-status.py
+++ display-status.py
@@ -32,6 +32,7 @@ def main():
     # Some other fonts: http://www.dafont.com/bitmap.php
     font = ImageFont.truetype('fonts/minecraftia-regular.ttf', 8)
     
+    last_seconds = None
     while True:
         try:
             # Draw a black filled box to clear the image.
@@ -39,11 +40,27 @@ def main():
     
             years, days, hours, minutes, seconds, microseconds = get_time_elapsed_elements()
             now_str = "%02dy %02dd %02d:%02d:%02d.%03d" % (years, days, hours, minutes, seconds, microseconds)
-            sync_str = "±%.2fus (%d satellites)" % (listener.pps_sync, listener.satellites_in_use)
+            sync_level_us = listener.sync_level
+            sync_level_str = "0.0s"
+            if sync_level_us > 0.0:
+                sync_level_str = ("%.2fms" % (sync_level_us / 1000.0)) if sync_level_us > 1000 else ("%.2fus" % sync_level_us)
+            sync_method_str = "no sync"
+            if listener.sync_method:
+                if listener.sync_method == "pps":
+                    sync_method_str = "%d satellites" % listener.satellites_in_use
+                elif listener.sync_method == "ntp":
+                    sync_method_str = "ntp"
+                
+            sync_str = "±%s (%s)" % (sync_level_str, sync_method_str)
+            is_new_second = (last_seconds is not None and seconds > last_seconds)
+            last_seconds = seconds
 
             draw.text((0, 0), "Longplayer", font=font, fill=255)
             draw.text((0, 10), now_str, font=font, fill=255)
             draw.text((0, 20), sync_str, font=font, fill=255)
+
+            if is_new_second:
+                draw.rectangle((126, 0, 128, 1), outline=255, fill=255)
 
             disp.getbuffer(image)
             disp.ShowImage()
--- oled_display/sync_stats.py
+++ oled_display/sync_stats.py
@@ -6,19 +6,32 @@ import subprocess
 import threading
 
 
-def get_pps_sync_us():
+def get_chrony_stats():
     lines = subprocess.check_output(["chronyc", "-c", "sources"]).decode()
     lines = lines.split("\n")
+    sync_method = None
+    sync_level = 0.0
+    ntp_servers = 0
     for line in lines:
         fields = line.split(",")
-        if len(fields) >= 8 and fields[2] == "PPS":
+        if len(fields) < 8:
+            continue
+        if fields[1] == "*":
             sync_level = float(fields[9]) * 1e6
-            return sync_level
+            if fields[0] == "#":
+                sync_method = "pps"
+            elif fields[0] == "^":
+                sync_method = "ntp"
+        if fields[0] == "^" and fields[1] == "+":
+            ntp_servers += 1
+    return sync_method, sync_level, ntp_servers
 
 class SyncStatsListener:
     def __init__(self):
         self.satellites_in_use = 0
-        self.pps_sync = 0
+        self.sync_method = None
+        self.sync_level = 0.0
+        self.ntp_servers = 0
         self.thread = threading.Thread(target=self.run)
         self.thread.daemon = True
         self.thread.start()
@@ -29,11 +42,11 @@ class SyncStatsListener:
         gps_socket.connect()
         gps_socket.watch()
         for gps_data in gps_socket:
+            self.sync_method, self.sync_level, self.ntp_servers = get_chrony_stats()
             if gps_data:
                 data_stream.unpack(gps_data)
                 gps_data = json.loads(gps_data)
                 if gps_data["class"] == "SKY":
                     self.satellites_in_use = sum(1 for sat in gps_data["satellites"] if sat.get("used"))
-                    self.pps_sync = get_pps_sync_us()
-                    print("Satellites in use: %d, PPS sync: %.3f" % (self.satellites_in_use, self.pps_sync))
+                    print("Sync method: %s, sync level: %.3fus, satellites in use: %d" % (self.sync_method, self.sync_level, self.satellites_in_use))
 
